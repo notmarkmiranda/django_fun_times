3a2788dcc4f763d1f3cdff5339b57b8a52fdf085
notmarkmiranda <notmarkmiranda@gmail.com>
2019-10-19 17:43:43 -0600

    setup article#show

--- articles/services.py
+++ articles/services.py
@@ -1,5 +1,6 @@
 import json
 import random
+from django.utils.dateparse import parse_datetime
 
 class ArticlesService:
     articles = []
@@ -8,17 +9,21 @@ class ArticlesService:
         with open('fixtures/content_api.json') as json_file:
             data = json.load(json_file)
             self.articles = data['results']
+            for article in self.articles:
+                article['path'] = article['path'][:-5] 
+                article['publish_at_with_time'] = parse_datetime(article['publish_at']).strftime('%B %-e, %Y at %I:%M%p')
+                article['publish_at'] = parse_datetime(article['publish_at']).strftime('%B %-e, %Y')
+                article['body'] = article['body'].replace('<p>{%sfr%}</p>', '')
             
     def find_featured_article(self, slug):
         featuredIndex = None
         for index in range(len(self.articles)):
-            self.articles[index]['path'] = self.articles[index]['path'][:-5]
             if self.articles[index]['uuid'] == 'a7acd8c8-c5ce-11e7-9fa6-0050569d4be0':
                 featuredIndex = index
         featuredArticle = self.articles.pop(featuredIndex)
         return { 'featured': featuredArticle, 'others': random.sample(self.articles, 3) }
     
-    def find_article(self, path):
+    def find_article_by_path(self, path):
         article = None
         for index in range(len(self.articles)):
             if path in self.articles[index]["path"]:
--- articles/views.py
+++ articles/views.py
@@ -4,17 +4,11 @@ from django.shortcuts import render
 from .services import ArticlesService
 
 class ArticlesIndexView(TemplateView):
-    template_name = 'articles_index.html'
+    def get(self, request):
+        all_articles = ArticlesService().find_featured_article('10-promise')
+        return render(request, 'articles_index.html', { 'featured': all_articles['featured'], 'articles': all_articles['others'], 'detail': False })
     
-    def featured(self):
-        return ArticlesService().find_featured_article('10-promise')['featured']
-        
-    def others(self):
-        return ArticlesService().find_featured_article('10-promise')['others']        
-        
 class ArticlesDetailView(TemplateView):
-    template_name = 'articles_detail.html'
-    
     def get(self, request, year, month, day, slug):
-        article = ArticlesService().find_article('%s/%s/%s/%s'%(year, month, day, slug))
-        return render(request, 'articles_detail.html', { 'article': article })
+        article = ArticlesService().find_article_by_path('%s/%s/%s/%s' % (year, month, day, slug))
+        return render(request, 'articles_detail.html', { 'article': article, 'detail': True })
