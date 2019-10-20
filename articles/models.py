de9276cbbba5ee88aae06566b4d5123363616636
notmarkmiranda <notmarkmiranda@gmail.com>
2019-10-20 12:21:57 -0600

    Add ability to comment on stories

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    article_uuid = models.CharField(max_length=50)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.body} -- {self.created_at}'
--- articles/views.py
+++ articles/views.py
@@ -1,16 +1,38 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 import json
 
 from .services import ArticlesService, QuotesService
+from .models import Comment
+from .forms import CommentForm
 
 class ArticlesIndexView(TemplateView):
     def get(self, request):
         all_articles = ArticlesService().find_featured_article('10-promise')
         return render(request, 'articles_index.html', { 'featured': all_articles['featured'], 'articles': all_articles['others'], 'detail': False })
     
-class ArticlesDetailView(TemplateView):
+class ArticlesDetailView(CreateView):
     def get(self, request, year, month, day, slug):
         article = ArticlesService().find_article_by_path('%s/%s/%s/%s' % (year, month, day, slug))
-        quotes = QuotesService().all_quotes()
-        return render(request, 'articles_detail.html', { 'article': article, 'detail': True, 'quotes': quotes[:3] })
+        comments = Comment.objects.filter
+        form = CommentForm()
+        return render(request, 'articles_detail.html', { 'article': article, 'comments': comments, 'form': form })
+    
+    def post(self, request, year, month, day, slug):
+        form = CommentForm(request.POST)
+        article = ArticlesService().find_article_by_path('%s/%s/%s/%s' % (year, month, day, slug))
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.article_uuid = article['uuid']
+            comment.save()
+            return redirect(reverse('article', kwargs={ 'year': year, 'month': month, 'day': day, 'slug': slug }))
+        return render(request, 'articles_detail.html', { 'article': article })
+        
+    def get_queryset(self):
+        return Comment.objects.filter(article_uuid='a7acd8c8-c5ce-11e7-9fa6-0050569d4be0').order_by('created_at')
+    
+    def get_absolute_url(self):
+        return reverse('home')
