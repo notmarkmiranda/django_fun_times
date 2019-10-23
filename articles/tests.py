de0708f9c93c6698025f07fb935d838c3a3c01d1
notmarkmiranda <notmarkmiranda@gmail.com>
2019-10-23 12:28:39 -0600

    add a base test, failiing because of lack of mocking?

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from unittest.mock import patch
 
-# Create your tests here.
+from .services import ArticlesService
+
+def methodStub(self, slug):
+    return slug
+
+class ArticlesIndexTest(SimpleTestCase):
+    @patch.object(ArticlesService, 'find_featured_article', methodStub)
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
